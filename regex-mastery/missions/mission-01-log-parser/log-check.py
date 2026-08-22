d813b0c03eddf6a30b070bcfaaa559abb8926d35
Axel Rodriguez <axelrodriguez4766@gmail.com>
2026-08-22 04:44:18 -0600

    feat(log-parser): funcion para parsear un log mediante input de usuario
    v1.0

--- regex-mastery/missions/mission-01-log-parser/log-check.py
+++ regex-mastery/missions/mission-01-log-parser/log-check.py
@@ -1,5 +1,18 @@
-import sys
 import re
 
-#Vamos a darle!
+def main():
+    print(parse_log(input("Log -> ")))
 
+def parse_log(s):
+    match = re.fullmatch(r'\[(INFO|ERROR|WARNING)] (\d{4}-\d{2}-\d{2}) (.+$)', s)
+
+    if not match:
+        raise ValueError()
+
+    return {
+        "level": match.group(1),
+        "date": match.group(2),
+        "message": match.group(3)
+    }
+
+main()
