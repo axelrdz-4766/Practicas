a2cb60f3917e76baab8635d79305ac40553be809
Axel Rodriguez <axelrodriguez4766@gmail.com>
2026-08-24 03:20:52 -0600

    feat(simpleMarkdownParser.py) se agrego un srcipt de python par apracticar regex para parsear de manera estricta delimitaciones de validacion
    v1.0

--- regex-mastery/missions/mission-02-simpleMarkdownParser/simpleMarkdownParser.py
+++ regex-mastery/missions/mission-02-simpleMarkdownParser/simpleMarkdownParser.py
@@ -1,3 +1,15 @@
 import re
 
-#Dale!!
+def main():
+    print(parse_markdown(input("Inp -> ")))
+
+def parse_markdown(text):
+    match = re.fullmatch(r'(\*\*|__|\*|_)([^\*_]|[^\*_].*)\1', text)
+
+    if not match:
+        raise ValueError
+
+    return match.group(2)
+
+if __name__ == "__main__":
+    main()
